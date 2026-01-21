43f95e4819b6ad6a9700d27e0c4aaf0a1e9dd488
c-nakashima <chisatonakashima1025@gmail.com>
2026-01-21 18:32:36 +1100

    fix: minor adjustments

--- components/work-details/DatavisDetail.tsx
+++ components/work-details/DatavisDetail.tsx
@@ -1,11 +1,35 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 // Datavis Detail Component
 export default function DatavisDetail() {
-  const images = Array.from({ length: 9 }, (_, i) => i + 1)
+  const images = Array.from({ length: 9 }, (_, i) => i + 1);
 
   return (
     <article className="flex justify-center items-center flex-col leading-relaxed text-neutral-800 space-y-12 py-12 px-4">
+      <p className="font-sans font-light w-full max-w-3xl align-left">
+        <a
+          href="http://togostanza.org/metastanza/index.html"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-indigo-700 hover:text-indigo-500 inline-flex items-center mr-3 gap-1"
+        >
+          Showcase - MetaStanza{" "}
+          <span aria-hidden className="text-xs mr-1">
+            ↗
+          </span>{" "}
+        </a>
+        <a
+          href="https://togostanza.github.io/metastanza/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-indigo-700 hover:text-indigo-500 inline-flex items-center gap-1"
+        >
+          List of Stanzas - MetaStanza{" "}
+          <span aria-hidden className="text-xs mr-1">
+            ↗
+          </span>{" "}
+        </a>
+      </p>
       {images.map((num) => (
         <figure key={num} className="w-full max-w-3xl">
           <Image
@@ -18,5 +42,5 @@ export default function DatavisDetail() {
         </figure>
       ))}
     </article>
-  )
+  );
 }
--- components/work-details/JessPortfolioDetail.tsx
+++ components/work-details/JessPortfolioDetail.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 // Jess Portfolio Detail Component
 export default function JessPortfolioDetail() {
-  const images = Array.from({ length: 5 }, (_, i) => i + 1)
+  const images = Array.from({ length: 5 }, (_, i) => i + 1);
 
   return (
     <article className="flex justify-center items-center flex-col leading-relaxed text-neutral-800 space-y-12 py-12 px-4">
+      <p className="font-sans font-light w-full max-w-3xl align-left">
+        <a
+          href="https://www.jessjaensch.com.au/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-indigo-700 hover:text-indigo-500 inline-flex items-center mr- gap-1"
+        >
+          Jess J Support Coordination Website{" "}
+          <span aria-hidden className="text-xs mr-1">
+            ↗
+          </span>{" "}
+        </a>
+      </p>
       {images.map((num) => (
         <figure key={num} className="w-full max-w-3xl">
           <Image
@@ -18,5 +31,5 @@ export default function JessPortfolioDetail() {
         </figure>
       ))}
     </article>
-  )
+  );
 }
